0386a90ebb0fa243c37c001ad7003b18b863147f
Johann BLANCHET <johann.blanchet@gmail.com>
2022-01-17 16:27:22 +0100

    New version with the implementation of the display of multipages TIFF fax format file

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# tiff_fax_format
+# tiff_fax_format_examples
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+This is a Flutter project created to illustrate examples to display TIFF Fax format file (single page and multiple pages).
+Theses examples use the DART 'image' library (from Brendan-Duncan https://github.com/brendan-duncan/image/)
+See a specific issue created about this topic: https://github.com/brendan-duncan/image/issues/363
--- lib/main.dart
+++ lib/main.dart
@@ -87,25 +87,49 @@ class _TiffFaxDisplayState extends State<TiffFaxDisplay> {
           title: Text(widget.title),
         ),
         body: SingleChildScrollView(
-          padding: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+          padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
           scrollDirection: Axis.vertical,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const Text('Click on the buttons to display TIFF file:'),
-                const SizedBox(height: 20.0),
+                const Text(
+                  'Click on the buttons to display TIFF file',
+                  style: TextStyle(color: Color.fromARGB(255, 255, 0, 0)),
+                ),
+                const SizedBox(height: 5.0),
+                Row(
+                  children: <Widget>[
+                    // ...
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Divider(
+                            color: Colors.black,
+                            height: 2.0,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10.0),
                 const Text('TIFF fax format single page:'),
                 ElevatedButton(
-                  child: const Text("File local in the app)"),
+                  child: const Text("Local file in the app"),
                   onPressed: () async {
                     log("START: single page (local): widgetToPrint='$widgetToPrint'");
                     String _fileToPrint = globalFileOutputDirLocal + globalFileNameTiffSinglePage;
                     log("START: single page (local): file to print: '$_fileToPrint'");
-                    UtilsLocalFile.localFileTiffLoad(directoryPath: globalFileOutputDirLocal, fileName: globalFileNameTiffSinglePage, isFromAssets: true)
+                    UtilsLocalFile.localFileTiffOnePageLoad(directoryPath: globalFileOutputDirLocal, fileName: globalFileNameTiffSinglePage, isFromAssets: true)
                         .then((value) {
                       setState(() {
-                        widgetToPrint = Image(image: value);
+                        widgetToPrint = Column(
+                          children: [
+                            const Text('Single page (local file in the app):'),
+                            Image(image: value),
+                          ],
+                        );
                         // log("INFO: single page (local): onPressed: widgetToPrint '$widgetToPrint'");
                         log("INFO: single page (local): onPressed: widgetToPrint modified!");
                       });
@@ -116,18 +140,23 @@ class _TiffFaxDisplayState extends State<TiffFaxDisplay> {
                     log("END: single page (local): onPressed");
                   },
                 ),
-                const SizedBox(height: 20.0),
+                const SizedBox(height: 5.0),
                 ElevatedButton(
                   child: const Text("Existing file in the phone"),
                   onPressed: () async {
                     log("START: single page (phone): widgetToPrint='$widgetToPrint'");
                     String _fileToPrint = globalFileOutputDirPhone + globalFileNameTiffSinglePage;
                     log("START: single page (phone): file to print: '$_fileToPrint'");
-                    UtilsLocalFile.localFileTiffLoad(directoryPath: globalFileOutputDirPhone, fileName: globalFileNameTiffSinglePage, isFromAssets: false)
+                    UtilsLocalFile.localFileTiffOnePageLoad(
+                            directoryPath: globalFileOutputDirPhone, fileName: globalFileNameTiffSinglePage, isFromAssets: false)
                         .then((value) {
                       setState(() {
-                        widgetToPrint = Image(image: value);
-                        // log("INFO: single page (local): onPressed: widgetToPrint '$widgetToPrint'");
+                        widgetToPrint = Column(
+                          children: [
+                            const Text('Single page (existing file in the phone):'),
+                            Image(image: value),
+                          ],
+                        );
                         log("INFO: single page (phone): onPressed: widgetToPrint modified!");
                       });
                     }).catchError((e) {
@@ -137,51 +166,115 @@ class _TiffFaxDisplayState extends State<TiffFaxDisplay> {
                     log("END: single page (phone): onPressed");
                   },
                 ),
-                widgetToPrint!,
-                const SizedBox(height: 40.0),
+                const SizedBox(height: 5.0),
+                Row(
+                  children: <Widget>[
+                    // ...
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Divider(
+                            color: Colors.black,
+                            height: 2.0,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10.0),
                 const Text('TIFF fax format multi pages:'),
                 ElevatedButton(
                   child: const Text("File local in the app)"),
                   onPressed: () async {
-                    log("START: single page (local): widget2ToPrint='$widget2ToPrint'");
+                    log("START: multipages (local): widget2ToPrint='$widget2ToPrint'");
                     String _fileToPrint = globalFileOutputDirLocal + globalFileNameTiffMultiPages;
-                    log("START: single page (local): file to print: '$_fileToPrint'");
+                    log("START: multipages (local): file to print: '$_fileToPrint'");
                     UtilsLocalFile.localFileTiffLoad(directoryPath: globalFileOutputDirLocal, fileName: globalFileNameTiffMultiPages, isFromAssets: true)
                         .then((value) {
+                      List<ImageProvider> listImagesInTiffFile = value;
                       setState(() {
-                        widget2ToPrint = Image(image: value);
-                        // log("INFO: single page (local): onPressed: widget2ToPrint '$widget2ToPrint'");
-                        log("INFO: single page (local): onPressed: widget2ToPrint modified!");
+                        widgetToPrint = Column(
+                          children: [
+                            const Text('Multipages (local file in the app):'),
+                            for (var imageToPrint in listImagesInTiffFile) Image(image: imageToPrint),
+                          ],
+                        );
+                        log("INFO: multipages (local): onPressed: widget2ToPrint modified!");
                       });
                     }).catchError((e) {
-                      log("ERROR: single page (local): onPressed: e='$e'");
+                      log("ERROR: multipages (local): onPressed: e='$e'");
                     });
 
-                    log("END: single page (local): onPressed");
+                    log("END: multipages (local): onPressed");
                   },
                 ),
-                const SizedBox(height: 20.0),
+                const SizedBox(height: 5.0),
                 ElevatedButton(
                   child: const Text("Existing file in the phone"),
                   onPressed: () async {
-                    log("START: single page (phone): widget2ToPrint='$widget2ToPrint'");
+                    log("START: multipages (phone): widget2ToPrint='$widget2ToPrint'");
                     String _fileToPrint = globalFileOutputDirPhone + globalFileNameTiffMultiPages;
-                    log("START: single page (phone): file to print: '$_fileToPrint'");
+                    log("START: multipages (phone): file to print: '$_fileToPrint'");
                     UtilsLocalFile.localFileTiffLoad(directoryPath: globalFileOutputDirPhone, fileName: globalFileNameTiffMultiPages, isFromAssets: false)
                         .then((value) {
+                      List<ImageProvider> listImagesInTiffFile = value;
                       setState(() {
-                        widget2ToPrint = Image(image: value);
-                        // log("INFO: single page (local): onPressed: widget2ToPrint '$widget2ToPrint'");
-                        log("INFO: single page (phone): onPressed: widget2ToPrint modified!");
+                        widgetToPrint = Column(
+                          children: [
+                            const Text('Multipages (existing file in the phone):'),
+                            for (var imageToPrint in listImagesInTiffFile) Image(image: imageToPrint),
+                          ],
+                        );
+                        log("INFO: multipages (local): onPressed: widget2ToPrint modified!");
                       });
                     }).catchError((e) {
-                      log("ERROR: single page (phone): onPressed: e='$e'");
+                      log("ERROR: multipages (phone): onPressed: e='$e'");
                     });
 
-                    log("END: single page (phone): onPressed");
+                    log("END: multipages (phone): onPressed");
                   },
                 ),
-                widget2ToPrint!,
+                const SizedBox(height: 5.0),
+                Row(
+                  children: <Widget>[
+                    // ...
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Divider(
+                            color: Colors.black,
+                            height: 2.0,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  'Area to print the content of the TIFF file:',
+                  style: TextStyle(color: Color.fromARGB(255, 255, 0, 0)),
+                ),
+                const SizedBox(height: 15.0),
+                widgetToPrint!,
+                const SizedBox(height: 5.0),
+                Row(
+                  children: <Widget>[
+                    // ...
+                    Expanded(
+                      child: Column(
+                        children: const [
+                          Divider(
+                            color: Colors.black,
+                            height: 2.0,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 5.0),
               ],
             ),
           ),
@@ -224,8 +317,75 @@ Future<File> writeByteDataToTempFile({required ByteData byteData, required Strin
 }
 
 class UtilsLocalFile {
-  // static Future<ImageProvider> localFileTiffLoad({ByteData? fileBytes, String? directoryPath, String? fileName}) async {
-  static Future<ImageProvider> localFileTiffLoad({required String directoryPath, required String fileName, required bool isFromAssets}) async {
+  static Future<List<ImageProvider>> localFileTiffLoad({required String directoryPath, required String fileName, required bool isFromAssets}) async {
+    ImageProvider imgFinalToReturn = const AssetImage('assets/images/image-broken.png');
+    File? _file;
+    String fileFromFullPath = "";
+    bool isFileExist = false;
+    ByteData? byteData;
+    fileFromFullPath = directoryPath + fileName;
+
+    if (isFromAssets) {
+      log("INFO: load file from assets path");
+
+      String fileToHandle = "assets/images" + fileFromFullPath;
+      log("INFO: fileToHandle='$fileToHandle'");
+      byteData = await rootBundle.load(fileToHandle);
+      log("INFO: byteData='${byteData.lengthInBytes}'");
+
+      _file = await writeByteDataToTempFile(byteData: byteData, destinationFileName: fileFromFullPath);
+    } else if (directoryPath != "" && fileName != "") {
+      log("INFO: load file from full path");
+      _file = File(fileFromFullPath);
+    }
+
+    List<ImageProvider> listImgToReturn = [];
+    isFileExist = await _file!.exists();
+    if (isFileExist) {
+      img.Image? imgToReturn;
+      List<int>? bytes;
+      try {
+        log("INFO: read bytes from file");
+        bytes = _file.readAsBytesSync();
+        // log("bytes='$bytes'");
+        log("INFO: bytes OK (NOT NULL)");
+      } catch (e) {
+        log("ERROR: during getting bytes: e='$e'");
+      }
+
+      if (bytes != null) {
+        try {
+          var decoder = img.TiffDecoder();
+          decoder.startDecode(bytes);
+          int numberFrames = decoder.numFrames();
+          Uint8List? bytesForMemoryImg;
+          for (int i = 0; i < numberFrames; i++) {
+            imgToReturn = decoder.decodeFrame(i)!;
+            if (imgToReturn is img.Image) {
+              log("INFO: STEP-DECODE: imgToReturn='$imgToReturn'");
+              bytesForMemoryImg = img.encodePng(imgToReturn) as Uint8List;
+              log("INFO: STEP-DECODE: bytesForMemoryImg.length='${bytesForMemoryImg.length}'");
+              imgFinalToReturn = MemoryImage(bytesForMemoryImg);
+              log("INFO: STEP-DECODE: imgFinalToReturn='$imgFinalToReturn'");
+              listImgToReturn.add(imgFinalToReturn);
+              log("INFO: STEP-DECODE: listImgToReturn='$listImgToReturn'");
+            } else {
+              log("ERROR: STEP-DECODE: Type of 'imgToReturn' IS NOT 'Image': imgToReturn='$imgToReturn'");
+            }
+          }
+          log("INFO: STEP-DECODE: listImgToReturn='$listImgToReturn'");
+        } catch (e) {
+          log("ERROR: STEP-DECODE: during decoding: e='$e'");
+        }
+      }
+    } else {
+      log("ERROR: File does not exist");
+    }
+
+    return listImgToReturn;
+  }
+
+  static Future<ImageProvider> localFileTiffOnePageLoad({required String directoryPath, required String fileName, required bool isFromAssets}) async {
     ImageProvider imgFinalToReturn = const AssetImage('assets/images/image-broken.png');
     File? _file;
     String fileFromFullPath = "";
